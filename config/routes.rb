108ab405daed31ae112122d9d49364f490a8c517
narumoto65 <narumoto65@gmail.com>
2019-11-08 00:06:30 +0900

    new answer page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :answers
   resources :users
+
+  get "/answers/new/:id", to: "answers#new", as: :user_new_answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_164137) do
+ActiveRecord::Schema.define(version: 2019_11_07_130338) do
+
+  create_table "answers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
